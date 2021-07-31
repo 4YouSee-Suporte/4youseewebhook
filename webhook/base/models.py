c2de05b96c08bb4298b997e5851e5dbd3d62de63
Alfonso <alfareiza@gmail.com>
2021-07-30 21:07:18 -0300

    mudança no banco. agora register é um foreign key de account

--- webhook/base/models.py
+++ webhook/base/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 
-# Create your models here.
-from webhook import settings
+class Account(models.Model):
+    name = models.CharField(max_length=128, primary_key=True, )
+    url = models.CharField(max_length=100, blank=True, default="-")
 
+    def __str__(self):
+        return f"{self.name}"
 
 class Register(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='records')
     nickname = models.CharField(max_length=128, )
     date = models.DateField()
     time = models.TimeField()
@@ -15,14 +19,7 @@ class Register(models.Model):
 
     class Meta:
         unique_together = ('date', 'time', 'player_id',)
+        ordering = ['date', 'time']
 
     def __str__(self):
         return f"{self.player_id} {self.date} - {self.time} - {self.media_id}"
-
-
-class Account(models.Model):
-    name = models.CharField(max_length=128, primary_key=True, )
-    register = models.ManyToManyField(Register, blank=True)
-
-    def __str__(self):
-        return f"{self.name}"
