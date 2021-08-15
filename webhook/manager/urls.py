29f322dfd18b1ba1843311b6049269abea711c31
Alfonso <alfareiza@gmail.com>
2021-08-14 21:57:34 -0300

    urls para atualizar dados, ver conta e home da app manager

---
+++ webhook/manager/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from webhook.manager import views
+
+app_name = 'manager'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<slug:slug>/', views.conta, name='conta'),
+    path(r'update/<slug:slug>/all', views.update_all_data_account, name='update_data_account'),
+    path(r'update/<slug:slug>/categories/', views.update_categories, name='update_categories'),
+    path(r'update/<slug:slug>/medias/', views.update_medias, name='update_medias'),
+    path(r'update/<slug:slug>/players/', views.update_players, name='update_players'),
+    path(r'update/<slug:slug>/playlists/', views.update_playlists, name='update_playlists')
+]
