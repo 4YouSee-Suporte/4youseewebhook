ba38ad77b2dfc81152fdb581ed598b8fd247e2c1
Alfonso <alfareiza@gmail.com>
2021-08-14 21:51:53 -0300

    adicionado date_added para a clase Client

--- webhook/base/models.py
+++ webhook/base/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 
 
-class Account(models.Model):
-    name = models.CharField(max_length=128, primary_key=True, )
-    url = models.CharField(max_length=100, blank=True, default="-")
+class Client(models.Model):
+    name = models.CharField(max_length=128)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
 
+
 class Register(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='records')
-    nickname = models.CharField(max_length=128, )
+    account = models.ForeignKey("manager.Account", on_delete=models.CASCADE, related_name='records')
+    nickname = models.CharField(max_length=128)
     date = models.DateField()
     time = models.TimeField()
     player_id = models.IntegerField()
     media_id = models.IntegerField()
-    media_type = models.CharField(max_length=2, )
+    media_type = models.CharField(max_length=2)
 
     class Meta:
-        unique_together = ('date', 'time', 'player_id',)
+        unique_together = ('date', 'time', 'player_id', 'nickname')
         ordering = ['date', 'time']
 
     def __str__(self):
-        return f"{self.player_id} {self.date} - {self.time} - {self.media_id}"
+        return f"{self.player_id} {self.player_id} {self.date} - {self.time} - {self.media_id}"
