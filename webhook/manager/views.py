c55cd6a1744b403d4183e95b34512d16cfafe4b8
Alfonso <alfareiza@gmail.com>
2021-08-18 14:24:17 -0300

    view personalizada para enviar mensagens ao template

--- webhook/manager/views.py
+++ webhook/manager/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -8,6 +9,7 @@ from django.utils.text import slugify
 from webhook.base.resources.inserts_to_bd import update_or_insert_players, update_or_insert_categories, \
     update_or_insert_playlists, update_or_insert_medias
 from webhook.base.resources.tools import name_of_account
+from webhook.manager.facade import qty_all_records
 from webhook.manager.forms import ContaNovaForm
 from webhook.manager.models import Account
 from webhook.manager.scripts.update_data import DataManager
@@ -25,16 +27,13 @@ def home(request):
         else:
             contas = Account.objects.all()
             return render(request, 'manager/index.html', {'form': form, 'contas': contas}, status=400)
-
     contas = Account.objects.all()
     return render(request, 'manager/index.html', {'contas': contas})
 
 
 def conta(request, slug):
     conta = get_object_or_404(Account, slug=slug)
-    total_records = len(conta.categories.all()) + len(conta.playlists.all()) + len(conta.players.all()) + len(
-        conta.medias.all())
-    ctx = {'conta': conta, 'total_records': total_records}
+    ctx = {'conta': conta, 'total_records': qty_all_records(conta)}
     return render(request, 'manager/conta.html', ctx)
 
 
@@ -42,10 +41,27 @@ def update_all_data_account(request, slug):
     conta = Account.objects.get(slug=slug)
     account = DataManager(conta.token)
     account.get_media_category(), account.get_medias(), account.get_playlists(), account.get_players()
-    update_or_insert_categories(conta, account.media_category)
-    update_or_insert_medias(conta, account.medias)
-    update_or_insert_playlists(conta, account.playlists)
-    update_or_insert_players(conta, account.players)
+    msg = ''
+    if not account.media_category:
+        msg += "• Não foi possível capturar informações das categorias."
+    else:
+        update_or_insert_categories(conta, account.media_category)
+
+    if not account.medias:
+        msg += "\n• Não foi possível capturar informações dos conteúdos."
+    else:
+        update_or_insert_medias(conta, account.medias)
+
+    if not account.playlists:
+        msg += "\n• Não foi possível capturar informações das playlists e por causa disso não podem ser obtidos os dados dos players."
+    elif not account.players:
+        update_or_insert_playlists(conta, account.playlists)
+        msg += "\n• Não foi possível capturar informações dos players."
+    else:
+        update_or_insert_playlists(conta, account.playlists)
+        update_or_insert_players(conta, account.players)
+
+    messages.error(request, msg)
     return redirect(
         conta)  # By passing some object to redirect(); that object’s get_absolute_url() method will be called to figure out the redirect URL
 
@@ -54,32 +70,52 @@ def update_players(request, slug):
     conta = Account.objects.get(slug=slug)
     account = DataManager(conta.token)
     account.get_players()
-    update_or_insert_players(conta, account.players)
-    return redirect(conta)
+    if not account.players:
+        messages.error(request, "Não foi possível capturar informações dos players.")
+        return redirect(conta)
+    else:
+        records = update_or_insert_players(conta, account.players)
+        if records:
+            return redirect(conta)
+        else:
+            messages.error(request, "Não foi possível capturar informações dos players.")
+            return redirect(conta)
 
 
 def update_categories(request, slug):
     conta = Account.objects.get(slug=slug)
     account = DataManager(conta.token)
     account.get_media_category()
-    update_or_insert_categories(conta, account.media_category)
-    return redirect(conta)
+    if not account.media_category:
+        messages.error(request, "Não foi possível capturar informações das categorias")
+        return redirect(conta)
+    else:
+        update_or_insert_categories(conta, account.media_category)
+        return redirect(conta)
 
 
 def update_playlists(request, slug):
     conta = Account.objects.get(slug=slug)
     account = DataManager(conta.token)
     account.get_playlists()
-    update_or_insert_playlists(conta, account.playlists)
-    return redirect(conta)
+    if not account.playlists:
+        messages.error(request, "Não foi possível capturar informações das playlists")
+        return redirect(conta)
+    else:
+        update_or_insert_playlists(conta, account.playlists)
+        return redirect(conta)
 
 
 def update_medias(request, slug):
     conta = Account.objects.get(slug=slug)
     account = DataManager(conta.token)
     account.get_medias()
-    update_or_insert_medias(conta, account.medias)
-    return redirect(conta)
+    if not account.medias:
+        messages.error(request, "Não foi possível capturar informações dos conteúdos")
+        return redirect(conta)
+    else:
+        update_or_insert_medias(conta, account.medias)
+        return redirect(conta)
 
 
 def delete(request, slug):
