dd4024cfdbb73397057510a465a65a4d98a0f0d5
Alfonso <alfareiza@gmail.com>
2021-09-14 14:57:56 -0500

    LOGIN_REDIRECT_URL aponta para '/'

--- webhook/settings.py
+++ webhook/settings.py
@@ -35,15 +35,15 @@ ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login/'
 
-LOGIN_REDIRECT_URL = '/manager/'
+LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
 
 # Recovery Password
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True)
+EMAIL_PORT = config('EMAIL_PORT', default=587)
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
